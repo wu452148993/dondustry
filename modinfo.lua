711d9f22f1fa7295762a550ef184995ac827202d
wu452148993 <wu452148993@yahoo.com>
2021-08-02 23:07:38 +0800

    init

---
+++ modinfo.lua
@@ -0,0 +1,29 @@
+name = "饥荒|工业"
+author = "wulf"
+version = "0.1"
+description = "Bring industry into Don't Starve Together\n"..
+"当前版本(ver)："..version..
+"\n本mod根据原版薇诺娜机器添加了电网系统,从而让工业化成为了可能"..
+"\n====已有设备===="..
+"\n--增强版发电机"..
+"\n--电路灯"..
+"\n--能量中继器"..
+"\n====待添加设备===="..
+"\n--充电墙"..
+"\n--充电炮塔"..
+"\n--待补充"..
+"\nmod还在开发中，大家有兴趣可以参与一起开发."..
+"\ngithub地址: https://github.com/wu452148993/dondustry.git"
+forumthread = ""
+dont_starve_compatible = false
+reign_of_giants_compatible = false
+shipwrecked_compatible = false
+api_version = 10
+dst_compatible = true
+client_only_mod = false
+all_clients_require_mod = true
+
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+server_filter_tags = {"tweak"}
+configuration_options = {}
---
+++ modmain.lua
@@ -0,0 +1,16 @@
+local Ingredient = GLOBAL.Ingredient
+local RECIPETABS = GLOBAL.RECIPETABS
+local STRINGS = GLOBAL.STRINGS
+local TECH = GLOBAL.TECH
+
+PrefabFiles = {
+	"winona_battery_high",
+	"winona_battery_low",
+	"winona_catapult",
+	"winona_spotlight"
+}
+
+Assets = {
+
+}
+--AddMinimapAtlas("minimap/xhx.xml")
